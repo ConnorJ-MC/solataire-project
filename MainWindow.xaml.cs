affb9d84de610bc1fbcd83869a5a1c3fea832cea
30202291 <30202291@my.nclan.ac.uk>
2026-03-16 09:42:20 +0000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace DraggableLabelDemo
+{
+    public partial class MainWindow : Window
+    {
+        private bool isDragging = false;
+        private Point mouseOffset;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                isDragging = true;
+                // Store the offset between mouse position and label's top-left corner
+                mouseOffset = e.GetPosition(MyLabel);
+                MyLabel.CaptureMouse();
+            }
+        }
+
+        private void Label_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                // Get mouse position relative to the parent container
+                Point position = e.GetPosition(MainCanvas);
+
+                // Move label so that the mouse stays at the same offset
+                Canvas.SetLeft(MyLabel, position.X - mouseOffset.X);
+                Canvas.SetTop(MyLabel, position.Y - mouseOffset.Y);
+            }
+        }
+
+        private void Label_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            MyLabel.ReleaseMouseCapture();
+        }
+    }
+}
