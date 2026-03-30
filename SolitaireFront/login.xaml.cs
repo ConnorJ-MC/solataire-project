613e7d13070bc75b030b29954965ba4270db4658
30202291 <30202291@my.nclan.ac.uk>
2026-03-30 11:01:34 +0100

    );

--- SolitaireFront/login.xaml.cs
+++ SolitaireFront/login.xaml.cs
@@ -169,8 +169,7 @@ namespace SolitaireFront
 
                     return;
                 }
-            }
-            ;
+            };
 
             loginManager.currentPlayer = loginManager.LoadPlayerData(first, last, selectedDate);
 
