ffaae5d07d381243323850c24c56f05e4d17fe37
30202291 <30202291@my.nclan.ac.uk>
2026-03-30 09:35:48 +0100

    tried to properly format the game page

---
+++ SolitaireFront/TableauOffsetConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Collections.Generic;
+using System.Globalization;
+using SolitaireFront;
+
+namespace SolitaireFront
+{
+    public class TableauOffsetConverter : IMultiValueConverter
+    {
+        public object Convert(Object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var card = values[0] as CardViewModel;
+            var pile = values[1] as IEnumerable<CardViewModel>;
+
+            if (card == null || pile == null)
+                return 0;
+
+            int index = pile.ToList().IndexOf(card);
+
+            if (!card.Model.isFaceUp)
+                return index * 20;
+
+            return index * 35;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType,  Object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
